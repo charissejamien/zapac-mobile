c5a4fffdb793d21d6fc0345644caf843da13e353
MerakiiZ <christlezoe4@gmail.com>
2026-06-13 21:59:15 +0800

    feat: made the favorites page component-based

---
+++ app/addFavorites/_layout.tsx
@@ -0,0 +1,5 @@
+import { Stack } from "expo-router";
+
+export default function AddFavoritesLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
---
+++ components/favorites/index.ts
@@ -0,0 +1,2 @@
+export { EmptyFavoritesState } from "./empty-favorites-state";
+export { FAVORITES_HEADER_COLOR, FavoritesHeader } from "./favorites-header";
