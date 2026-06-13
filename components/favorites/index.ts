c38d697421d67b0f5642e2a05bc6bfdff086bd2c
MerakiiZ <christlezoe4@gmail.com>
2026-06-13 22:58:41 +0800

    feat: added add favorite routes (without buttons yet)

--- components/favorites/index.ts
+++ components/favorites/index.ts
@@ -1,2 +1,3 @@
 export { EmptyFavoritesState } from "./empty-favorites-state";
+export { FavoriteRouteButton } from "./favorite-route-button";
 export { FAVORITES_HEADER_COLOR, FavoritesHeader } from "./favorites-header";
